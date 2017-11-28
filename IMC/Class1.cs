9d31d2598f15913c453d8801df3011c83a8f3109
SG <sophie.girard@ac-grenoble.fr>
2017-11-28 10:58:02 +0100

    Ajout d'une classe

---
+++ IMC/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMC
+{
+    class Class1
+    {
+        //comment
+        private int id;
+        private string nom;
+    }
+}
